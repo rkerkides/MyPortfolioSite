42b936b0697621e32ec23d0a5ad1da74000e3ccb
Renos Kerkides <rkerkides@gmail.com>
2024-02-12 01:26:09 +0000

    Add feature to sort projects

--- projects/views.py
+++ projects/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from .models import Project
 
 def projects(request):
-    projects_list = Project.objects.order_by('-date_created')
+    # Default sorting is by date created in descending order
+    sort_by = request.GET.get('sort', 'date_created')
+    order = request.GET.get('order', 'desc')  # Get the order from the query parameters
+
+    # A dictionary mapping sort options to model fields
+    sort_options = {
+        'date_created': 'date_created',
+        'last_updated': 'last_updated',
+        'technologies': 'technology',
+    }
+    
+    sort_field = sort_options.get(sort_by, 'date_created')
+    if order == 'asc':
+        projects_list = Project.objects.order_by(sort_field)
+    else:  # Default to descending if anything other than 'asc' is specified
+        projects_list = Project.objects.order_by(f'-{sort_field}')
+    
     return render(request, 'projects/projects.html', {'projects': projects_list})
