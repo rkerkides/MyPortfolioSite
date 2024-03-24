f0f0fdec1d0a4c5f2c022276451e94f7dd20b8e0
Renos Kerkides <rkerkides@gmail.com>
2024-03-24 15:36:41 +0000

    comment out blog-related code

--- portfoliofromscratch/urls.py
+++ portfoliofromscratch/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'), name='main'),
     path('projects/', include('projects.urls'), name='projects'),
-    path('blog/', include('blog.urls'), name='blog'),
+    # path('blog/', include('blog.urls'), name='blog'),
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
